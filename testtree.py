f2de06074d9efcd6af638cfe02467e9a9dca0374
yxw <649809052@qq.com>
2023-01-16 16:43:39 +0800

    v6(oct half)

---
+++ testtree.py
@@ -0,0 +1,15 @@
+import torch
+import svox
+
+tree=svox.N3Tree()
+#t是size(8,4)的一个数据结构
+a =0
+for i in range(8):
+    tree[i] = 0
+    tree[i] = a=a+1
+b=torch.tensor([[0.1026, -0.3812, -0.9188],[ 0.1026, -0.3812, -0.9188]])
+a=torch.tensor([[0.0210, 1.0595, 0.8249],[0.0210, 1.0595, 0.8249]])
+
+ren = svox.VolumeRenderer(tree)
+ray = svox.Rays(a,b,b)
+print(ren(ray))
